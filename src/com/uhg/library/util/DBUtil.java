09a161196a430488f8690e1fc958d4b2d28ea8c4
Madhu <f2013152@goa.bits-pilani.ac.in>
2017-07-28 16:40:46 +0530

    AWS connection added

--- src/com/uhg/library/util/DBUtil.java
+++ src/com/uhg/library/util/DBUtil.java
@@ -3,24 +3,49 @@ package com.uhg.library.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class DBUtil {
-	public static Connection con=null;
-	static{
+public static Connection getConnection(){
+	Connection con=null;
+	
 		try{
 //			Class.forName("oracle.jdbc.driver.OracleDriver");
 //			con=DriverManager.getConnection("jdbc:oracle:thin:@librarymanagementdb.cvhmku11tg1d.us-east-2.rds.amazonaws.com:1521","Scott","tiger123");
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			String hostname="librarymanagementdb.cvhmku11tg1d.us-east-2.rds.amazonaws.com";
+			String port="1521";
+			String dbName="ORCL";
+			String userName="Scott";
+			String password="ABCDQWERTY";
+				//String driver="oracle:thin";
+			String jdbcUrl = "jdbc:oracle:thin://" + hostname + ":" +port + "/" + dbName;
+					//+ " "?user=" + userName + "&password=" + password;
+			con=DriverManager.getConnection(jdbcUrl,userName,password);
 		}
 		catch(Exception e)
 		{
+			System.out.println("catch");
 			e.printStackTrace();
 		}
+		return con;
 	}
-		public static Connection getConnection(){
-			return con;
-	
+		public Date convertStringToDate(String dateInString) {
+		Date date = null;
+		DateFormat dateFormat = null;
+		try {
+			dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+			date = dateFormat.parse(dateInString);
 		}
- 
+		catch (ParseException e) {
+		e.printStackTrace();
+			}
+		return date;
+}
+			
     public static void closeConnection( Connection toBeClosed ) {
         if( toBeClosed == null )
             return;
@@ -31,4 +56,3 @@ public class DBUtil {
         }
     }
 }
-
